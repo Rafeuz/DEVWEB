23628b5a796ecb7eb4ce731a82aab02f166677ff
RAFAEL CRISOSTOMO DA SILVA <rafael.crisostomo@academico.ifrn.edu.br>
2019-09-05 14:20:17 -0300

    att

--- testp2/blog/models.py
+++ testp2/blog/models.py
@@ -12,6 +12,6 @@ class Postagem(models.Model):
 class Comentario(models.Model):
 	autor = models.CharField(max_length = 200);
 	texto = models.CharField(max_length = 200);
-	postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
+	postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE, null=True)
 	def __str__(self):
 		return self.texto	
