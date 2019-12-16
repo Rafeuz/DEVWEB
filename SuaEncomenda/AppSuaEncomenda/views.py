08e6d0e1a18d00ceffdb2a7954fb24512231f06a
RAFAEL CRISOSTOMO DA SILVA <rafael.crisostomo@academico.ifrn.edu.br>
2019-12-16 17:25:53 -0300

    apresentação

--- SuaEncomenda/AppSuaEncomenda/views.py
+++ SuaEncomenda/AppSuaEncomenda/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
 from django.urls import reverse
+from django.contrib import messages
 from .models import Encomenda
 
 
--- SuaEncomenda/accounts/signals.py
+++ SuaEncomenda/accounts/signals.py
@@ -4,11 +4,11 @@ from django.dispatch import receiver
 from .models import Perfil
 
 
-@receiver(post_save, sender=User)
-def criar_perfil(sender, instance, created, **kwargs):
-	if created:
-		Perfil.objects.create(user=instance)
+#@receiver(post_save, sender=User)
+#def criar_perfil(sender, instance, created, **kwargs):
+#	if created:
+#		Perfil.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def salvar_perfil(sender, instance, **kwargs):
-	instance.perfil.save()
+#@receiver(post_save, sender=User)
+#def salvar_perfil(sender, instance, **kwargs):
+#	instance.perfil.save()
